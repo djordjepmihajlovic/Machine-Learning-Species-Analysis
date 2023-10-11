3c2669da5b43b1e51260e4de0712f4a11d5a56b8
Djordje Mihajlovic <djdjdjordje@gmail.com>
2023-10-11 10:42:24 +0100

    general structure + ideas.txt

--- species/main.py
+++ species/main.py
@@ -1,9 +1,9 @@
 # copy pasted from explore_species_data.py; use this file to edit and test so explore_species_data.py is clean
 
-import os
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+from models import FFNNet
+import seaborn as sn
 
 
 data = np.load('species_train.npz')
@@ -17,15 +17,44 @@ data_test = np.load('species_test.npz', allow_pickle=True)
 test_locs = data_test['test_locs']   
 test_pos_inds = dict(zip(data_test['taxon_ids'], data_test['test_pos_inds']))    
 
-# data stats
-print('Train Stats:')
-print('Number of species in train set:           ', len(species))
-print('Number of train locations:                ', train_locs.shape[0])
-_, species_counts = np.unique(train_ids, return_counts=True)
-print('Average number of locations per species:  ', species_counts.mean())
-print('Minimum number of locations for a species:', species_counts.min())
-print('Maximum number of locations for a species:', species_counts.max())
+# below is code structure for implementing model defined in models.py
 
-print(train_locs[0])
+# X = torch.rand((28, 28))
 
+# output = net(X.view(-1, 28*28)) #-1?
 
+# # loss and optimization
+
+# optimizer = optim.Adam(net.parameters(), lr = 0.001) #learning rate = size of steps to take (try find 'lowest minima')
+
+# EPOCHS = 3
+
+# for epoch in range(EPOCHS):
+#     for data in trainset:
+#         # data is a batch of features and labels
+#         X, y = data
+#         net.zero_grad()  # note: pytorch!
+#         output = net(X.view(-1, 28*28))
+#         loss = F.nll_loss(output, y) # note: one-hot vector [0, 0, 1, 0] use mean sq
+#         loss.backward() # backpropagation ***
+#         optimizer.step() # adjust weights
+
+#     print(loss)
+
+# correct = 0
+# total = 0
+
+# with torch.no_grad():
+#     for data in testset:
+#         X, y = data
+#         output = net(X.view(-1, 784))
+#         for idx, i in enumerate(output):
+#             if torch.argmax(i) == y[idx]:
+#                 correct += 1
+#             total +=1
+# print("Accuracy: ", round(correct/total, 3))
+
+# plt.imshow(X[1].view(28,28))
+# plt.show()
+
+# print(torch.argmax(net(X[1].view(-1,784))[0]))
