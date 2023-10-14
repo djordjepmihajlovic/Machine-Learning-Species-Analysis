8f58511743d8f4db8a90e910b19237addf42e926
jtrossiter <147512180+jtrossiter@users.noreply.github.com>
2023-10-14 10:59:32 +0100

    Added country identification

--- species/locs.py
+++ species/locs.py
@@ -1,4 +1,5 @@
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="s2572047@ed.ac.uk")
+geolocator = Nominatim(user_agent="youremail@provider")
 location = geolocator.reverse("55.92186485133942, -3.1742764925776212")
 print(location.address)
+print(location.raw['address']['country'])
