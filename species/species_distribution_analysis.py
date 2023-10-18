510ac85f5cf9fd5b07e6f5d44b6b2481d977d24c
Djordje Mihajlovic <djdjdjordje@gmail.com>
2023-10-18 11:58:09 +0100

    fixed functions - still being worked on

--- species/species_distribution_analysis.py
+++ species/species_distribution_analysis.py
@@ -1,3 +1,6 @@
+# for density run in terminal as: python species_distribution_analysis.py -d density
+# change arg as needed i.e. -d distribution
+
 import numpy as np
 import csv
 from pathlib import Path
@@ -6,91 +9,170 @@ import random
 import geopandas as gpd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
+import math
+from argparse import ArgumentParser
 
-def distance(x1, y1, x2, y2):
-    return ((x2-x1)**2 + (y2-y1)**2)**(0.5)
+def distance(x1, y1, x2, y2): 
+    # need to alter this as distances n in longitude (xn) are not consistent (rather vary as a function of latitude)
+    # also sin(theta/2) function because longitude wraps round itself i.e. max longitude dist at x separation of 180
+    # may need further checking cause I think its wrong somewhere...
+    y1_ang = math.radians(y1) 
+    y2_ang = math.radians(y2)
+    x1_ang = math.radians(x1)
+    x2_ang = math.radians(x2)
+    return ((math.sin(0.5*(x2_ang-x1_ang))*((x2*math.cos(y2_ang))-(x1*math.cos(y1_ang))))**2 + (y2-y1)**2)**(0.5)
 
-# load train data
-data = np.load('species_train.npz')
-train_locs = data['train_locs']          
-train_ids = data['train_ids']               
-species = data['taxon_ids']      
-species_names = dict(zip(data['taxon_ids'], data['taxon_names'])) 
+def get_params(): # runs chosen problem
+    par = ArgumentParser()
+    par.add_argument("-d", "--problem", type=str, default="distribution", help="Options: distribution, density, ...")
+    args = par.parse_args()
 
-# loading test data 
-data_test = np.load('species_test.npz', allow_pickle=True)
-test_locs = data_test['test_locs']
-test_pos_inds = dict(zip(data_test['taxon_ids'], data_test['test_pos_inds']))   
+    return args
 
-species_train_dist = Path("species_train_dist.csv")
+def main():
+    # load train data
+    data = np.load('species_train.npz')
+    train_locs = data['train_locs']          
+    train_ids = data['train_ids']               
+    species = data['taxon_ids']      
+    species_names = dict(zip(data['taxon_ids'], data['taxon_names'])) 
 
-if species_train_dist.is_file() == False: # need to create the data
+    # loading test data 
+    data_test = np.load('species_test.npz', allow_pickle=True)
+    test_locs = data_test['test_locs']
+    test_pos_inds = dict(zip(data_test['taxon_ids'], data_test['test_pos_inds']))   
 
-    species_distn = []
-    count = 0
+    species_train_dist = Path("species_train_dist.csv")
+    species_train_dense = Path("species_train_dense.csv")
 
-    for sp in species: # for loop to estimate largest distance between datapoints for a species
-        test_inds_pos = test_pos_inds[sp]  
-        dist = []
-        # note: some species have a lot more data than others!
-        # hence: going to artificially select a maximum of 500 data points to do this testing. imo should be enough
-        if len(test_inds_pos)>500:
-            test_inds_pos = random.sample(test_inds_pos, 500)
-        for x in test_inds_pos:
-            for y in test_inds_pos:
-                if x != y:
-                    val = distance(test_locs[x, 1], test_locs[x, 0], test_locs[y, 1], test_locs[y, 0])
-                    dist.append(val)
+    if species_train_dist.is_file() == False or species_train_dense.is_file() == False: # need to create the data
 
-        species_distn.append([max(dist), sp])
-        count += 1
-        print(f"species {species_names[sp]} done!, max distribution: {max(dist)}, {count} out of 500")
+        species_distn = []
+        species_dense = []
+        count = 0
 
-    with open('species_train_dist.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(species_distn) # saves in file if file didn't exist
+        for sp in species: # for loop to estimate largest distance between datapoints for a species
+            test_inds_pos = test_pos_inds[sp]  
+            dist = []
+            # note: some species have a lot more data than others!
+            # hence: going to artificially select a maximum of 500 data points to do this testing. imo should be enough
+            if len(test_inds_pos)>500:
+                test_inds_pos = random.sample(test_inds_pos, 500)
+            for x in test_inds_pos:
+                for y in test_inds_pos:
+                    if x != y:
+                        # nb. [x coord. latitude(0), y coord. longitude(1)]
+                        val = distance(test_locs[x, 0], test_locs[x, 1], test_locs[y, 0], test_locs[y, 1])
+                        dist.append(val)
 
-# now can analyze generated data
+            species_distn.append([max(dist), sp])
+            species_dense.append([sum(dist)/len(dist), sp])
+            count += 1
+            print(f"species {species_names[sp]} done!, max distribution: {max(dist)}, {count} out of 500")
 
-with open('species_train_dist.csv', newline='') as f:
-    reader = csv.reader(f)
-    species_distn = list(reader)
+        with open('species_train_dist.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(species_distn) # saves in file if file didn't exist - now function doesn't need to be run
 
-sorted_species_distn = sorted(species_distn, key=lambda x: float(x[0]))
+        with open('species_train_dense.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(species_dense) # saves in file if file didn't exist - now function doesn't need to be run
 
-label_mini = int(sorted_species_distn[0][1])
-label_maxi = int(sorted_species_distn[-1][1])
+    # now can analyze generated data
 
-print(f"The largest distributed species is: {species_names[label_maxi]}") # finds label and species with largest spread
-print(f"The smallest distributed species is: {species_names[label_mini]}") # ''' smallest '''
+    if d == "distribution":
 
+        with open('species_train_dist.csv', newline='') as f:
+            reader = csv.reader(f)
+            species_distn = list(reader)
 
-test_inds_pos_maxi = test_pos_inds[label_maxi]  
+        sorted_species_distn = sorted(species_distn, key=lambda x: float(x[0]))
 
-test_inds_pos_mini = test_pos_inds[label_mini]
+        label_mini = int(sorted_species_distn[0][1])
+        label_maxi = int(sorted_species_distn[-1][1])
 
-# geopandas code to plot data
-
-geometry_maxi = [Point(xy) for xy in zip(test_locs[test_inds_pos_maxi, 1], test_locs[test_inds_pos_maxi, 0])] 
-geometry_mini = [Point(xy) for xy in zip(test_locs[test_inds_pos_mini, 1], test_locs[test_inds_pos_mini, 0])]
-
-gdf_maxi = GeoDataFrame(geometry=geometry_maxi) 
-gdf_mini = GeoDataFrame(geometry=geometry_mini)
-
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres')) 
-ax = world.plot(figsize=(10, 6))
-gdf_maxi.plot(ax=ax, marker='o', color='b', markersize=5, label=f"{species_names[label_maxi]} ({label_maxi})")
-gdf_mini.plot(ax=ax, marker='o', color='r', markersize=5, label=f"{species_names[label_mini]} ({label_mini})")
-
-plt.legend()
-plt.title(f"Population distribution of most sparse vs. most spread species.")
-plt.show()
+        print(f"The largest distributed species is: {species_names[label_maxi]}") # finds label and species with largest spread
+        print(f"The smallest distributed species is: {species_names[label_mini]}") # ''' smallest '''
 
 
+        test_inds_pos_maxi = test_pos_inds[label_maxi]  
+
+        test_inds_pos_mini = test_pos_inds[label_mini]
+
+        # geopandas code to plot data
+
+        geometry_maxi = [Point(xy) for xy in zip(test_locs[test_inds_pos_maxi, 1], test_locs[test_inds_pos_maxi, 0])] 
+        geometry_mini = [Point(xy) for xy in zip(test_locs[test_inds_pos_mini, 1], test_locs[test_inds_pos_mini, 0])]
+
+        gdf_maxi = GeoDataFrame(geometry=geometry_maxi) 
+        gdf_mini = GeoDataFrame(geometry=geometry_mini)
+
+        world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres')) 
+        ax = world.plot(figsize=(10, 6))
+        gdf_maxi.plot(ax=ax, marker='o', color='b', markersize=5, label=f"{species_names[label_maxi]} ({label_maxi})")
+        gdf_mini.plot(ax=ax, marker='o', color='r', markersize=5, label=f"{species_names[label_mini]} ({label_mini})")
+
+        plt.legend()
+        plt.title(f"Population distribution of most localized vs. most spread species.")
+        plt.show()
+
+    elif d == "density":
+
+        with open('species_train_dense.csv', newline='') as f:
+            reader = csv.reader(f)
+            species_dense = list(reader)
+
+        density = []
+
+        for x in species_dense:
+            area = math.pi*(float(x[0])**2)
+            count = len(test_pos_inds[int(x[1])]) 
+
+            density.append([count/area, int(x[1])])
+
+        sorted_species_density = sorted(density, key=lambda x: float(x[0]))
+
+        label_mini = int(sorted_species_density[0][1])
+        label_maxi = int(sorted_species_density[-1][1])
+
+        print(f"The most densely populated species is: {species_names[label_maxi]}") # finds label and species with highest count per area
+        print(f"The least densely populated species is: {species_names[label_mini]}") # ''' smallest '''
+
+
+        test_inds_pos_maxi = test_pos_inds[label_maxi]  
+
+        test_inds_pos_mini = test_pos_inds[label_mini]
+
+        # geopandas code to plot data
+
+        geometry_maxi = [Point(xy) for xy in zip(test_locs[test_inds_pos_maxi, 1], test_locs[test_inds_pos_maxi, 0])] 
+        geometry_mini = [Point(xy) for xy in zip(test_locs[test_inds_pos_mini, 1], test_locs[test_inds_pos_mini, 0])]
+
+        gdf_maxi = GeoDataFrame(geometry=geometry_maxi) 
+        gdf_mini = GeoDataFrame(geometry=geometry_mini)
+
+        world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres')) 
+        ax = world.plot(figsize=(10, 6))
+        gdf_maxi.plot(ax=ax, marker='o', color='b', markersize=5, label=f"{species_names[label_maxi]} ({label_maxi}), density: {float(sorted_species_density[-1][0])}")
+        gdf_mini.plot(ax=ax, marker='o', color='r', markersize=5, label=f"{species_names[label_mini]} ({label_mini}), density: {float(sorted_species_density[0][0])}")
+
+        plt.legend()
+        plt.title(f"Population distribution of most vs. least densely populated species.")
+        plt.show()
 
 
 
-##### Below commented out is way to find countries of data
+
+if __name__ == "__main__":
+    args = get_params()
+    d = args.problem
+
+    main()
+
+
+
+##### Below commented out is way to find countries of data #####
+
 
 # get train locations and plot
 # train_inds_pos = np.where(train_ids == sp)[0]
