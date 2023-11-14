e3fedb5d5a75a00e210da1375bd2262ae068f8dc
miguelpmerelles <miguelperezmerelles8@gmail.com>
2023-11-14 10:16:47 +0000

    Analysis

--- species/random_forest.py
+++ species/random_forest.py
@@ -76,10 +76,10 @@ test_pos_inds = dict(zip(data_test['taxon_ids'], data_test['test_pos_inds']))
 with open('reverse_dict.pkl', 'rb') as file:
     reverse_test_pos_inds = pickle.load(file)
 
-rdf = RandomForestClassifier(n_estimators = 10, criterion = 'gini')#, max_depth= 20) #Change max_depth /min_sample_leaf
+rdf = RandomForestClassifier(n_estimators = 100, criterion = 'gini', max_depth = 15)
 #############################################################################################################################################
 # Should keep working on best parameters for max_depth and eventually do it with 100 estimators.
-
+# Using max depth = 18, from graph I think 15 could be argued as good accuracy without over fitting too much
 #############################################################################################################################################
 rdf.fit(new_train_locs, new_train_ids)
 
@@ -92,8 +92,6 @@ for index in range(len(test_locs)):
     test_id = reverse_test_pos_inds.get(index)
     test_ids.append(test_id)
 
-
-sp_iden = [12716, 4535, 35990, 13851, 43567]
 """
 threshold = 0.001
 TPR_list = []
@@ -126,7 +124,23 @@ print(TPR_list)
 print(FPR_list)
 print(Precision_list)
 """
+
 #Using Djordies method.. trying to implment my version of it.
+
+#top 5 sparsest species: [4345, 44570, 42961, 32861, 2071]
+#top 5 densest species: [38992, 29976, 8076, 145310, 4569]
+#top 5 largest distn species: [4208, 12716, 145300, 4636, 4146]
+#top 5 smallest distn species: [35990, 64387, 73903, 6364, 27696]
+sp_iden = [12716, 4535, 35990, 13851, 43567]
+
+most_sparse = [4345, 44570, 42961, 32861, 2071]
+most_dense =  [38992, 29976, 8076, 145310, 4569]
+larg_dist = [4208, 12716, 145300, 4636, 4146]
+small_dist = [35990, 64387, 73903, 6364, 27696]
+
+#max_species = [most_sparse, most_dense, larg_dist, small_dist]
+
+#First doing most sparse species, I am going to do each species as before an then an average (print + graph)
 
 true_p = np.zeros((5, 20))
 true_n = np.zeros((5, 20))
@@ -135,7 +149,7 @@ false_n = np.zeros((5, 20))
 total = np.zeros((5, 20))
 
 j = 0
-for id in sp_iden:
+for id in most_dense: #####
     id_inx = np.where(species == id)
     for i in range(len(test_locs)):
         for idx, thr in enumerate(np.linspace(0.0, 0.05, 20)): #Maybe use 4 or 5 to start?
@@ -157,23 +171,6 @@ false_p_rate = false_p/(true_n + false_p)
 precision = true_p/(true_p+false_p)
 recall = true_p_rate
 
-prec1 = precision[2].tolist()
-rec1 = recall[2].tolist()
-prec2 = precision[3].tolist()
-rec2 = recall[3].tolist()
-
-test_locs_id = test_pos_inds.get(35990)
-print(test_locs_id)
-print(prec1)
-print(true_p_rate[2].tolist())
-print(rec1)
-
-plt.plot(rec1, prec1)
-plt.show()
-
-plt.plot(rec2, prec2)
-plt.show()
-
 AUC = []
 
 AUC.append(np.abs(np.trapz(y=true_p_rate[0].tolist(), x=false_p_rate[0].tolist())))
@@ -182,7 +179,11 @@ AUC.append(np.abs(np.trapz(y=true_p_rate[2].tolist(), x=false_p_rate[2].tolist()
 AUC.append(np.abs(np.trapz(y=true_p_rate[3].tolist(), x=false_p_rate[3].tolist())))
 AUC.append(np.abs(np.trapz(y=true_p_rate[4].tolist(), x=false_p_rate[4].tolist())))
 
-print(AUC)
+print('ROC AUC values are: ', AUC)
+
+mean_AUC_ROC = sum(AUC)/len(AUC)
+
+print('Mean ROC AUC is', mean_AUC_ROC)
 
 PR_AUC = []
 
@@ -192,21 +193,339 @@ PR_AUC.append(np.abs(np.trapz(y=precision[2].tolist(), x=recall[2].tolist())))
 PR_AUC.append(np.abs(np.trapz(y=precision[3].tolist(), x=recall[3].tolist())))
 PR_AUC.append(np.abs(np.trapz(y=precision[4].tolist(), x=recall[4].tolist())))
 
-print(PR_AUC)
+print('PR AUC values are: ', PR_AUC)
 
-sns.barplot(x=sp_iden, y=AUC, color='b')
+mean_AUC_PR = sum(PR_AUC)/len(PR_AUC)
+
+print('Mean PR AUC is', mean_AUC_PR)
+
+sns.barplot(x=most_dense, y=AUC, color='b')########
 plt.xlabel('Species')
 plt.ylabel('AUC-ROC')
 plt.show()
 
-sns.barplot(x=sp_iden, y=PR_AUC, color='r')
+sns.barplot(x=most_dense, y=PR_AUC, color='r')########
 plt.xlabel('Species')
 plt.ylabel('AUC-Precision-Recall')
 plt.ylim(0, 1)
 plt.show()
 
+sns.barplot(x=['mean_AUC_ROC', 'mean_AUC_PR'], y=[mean_AUC_ROC, mean_AUC_PR], color='r')
+plt.xlabel('Most Dense Averages')###############
+plt.ylabel('AUC')
+plt.ylim(0, 1)
+plt.show()
+
+
+true_p = np.zeros((5, 20))
+true_n = np.zeros((5, 20))
+false_p = np.zeros((5, 20))
+false_n = np.zeros((5, 20))
+total = np.zeros((5, 20))
+
+j = 0
+for id in larg_dist: #####
+    id_inx = np.where(species == id)
+    for i in range(len(test_locs)):
+        for idx, thr in enumerate(np.linspace(0.0, 0.05, 20)): #Maybe use 4 or 5 to start?
+            if id in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                true_p[j][idx] += 1
+            elif id in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                false_n[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                false_p[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                true_n[j][idx] += 1
+    j += 1
+    print(f"Species {j} done.")
+    
+
+
+true_p_rate = true_p/(true_p + false_n)
+false_p_rate = false_p/(true_n + false_p)
+precision = true_p/(true_p+false_p)
+recall = true_p_rate
+
+AUC = []
+
+AUC.append(np.abs(np.trapz(y=true_p_rate[0].tolist(), x=false_p_rate[0].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[1].tolist(), x=false_p_rate[1].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[2].tolist(), x=false_p_rate[2].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[3].tolist(), x=false_p_rate[3].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[4].tolist(), x=false_p_rate[4].tolist())))
+
+print('ROC AUC values are: ', AUC)
+
+mean_AUC_ROC = sum(AUC)/len(AUC)
+
+print('Mean ROC AUC is', mean_AUC_ROC)
+
+PR_AUC = []
+
+PR_AUC.append(np.abs(np.trapz(y=precision[0].tolist(), x=recall[0].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[1].tolist(), x=recall[1].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[2].tolist(), x=recall[2].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[3].tolist(), x=recall[3].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[4].tolist(), x=recall[4].tolist())))
+
+print('PR AUC values are: ', PR_AUC)
+
+mean_AUC_PR = sum(PR_AUC)/len(PR_AUC)
+
+print('Mean PR AUC is', mean_AUC_PR)
+
+sns.barplot(x=larg_dist, y=AUC, color='b')########
+plt.xlabel('Species')
+plt.ylabel('AUC-ROC')
+plt.show()
+
+sns.barplot(x=larg_dist, y=PR_AUC, color='r')########
+plt.xlabel('Species')
+plt.ylabel('AUC-Precision-Recall')
+plt.ylim(0, 1)
+plt.show()
+
+sns.barplot(x=['mean_AUC_ROC', 'mean_AUC_PR'], y=[mean_AUC_ROC, mean_AUC_PR], color='r')
+plt.xlabel('Largest Distance Averages')###############
+plt.ylabel('AUC')
+plt.ylim(0, 1)
+plt.show()
+
+true_p = np.zeros((5, 20))
+true_n = np.zeros((5, 20))
+false_p = np.zeros((5, 20))
+false_n = np.zeros((5, 20))
+total = np.zeros((5, 20))
+
+j = 0
+for id in small_dist: #####
+    id_inx = np.where(species == id)
+    for i in range(len(test_locs)):
+        for idx, thr in enumerate(np.linspace(0.0, 0.05, 20)): #Maybe use 4 or 5 to start?
+            if id in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                true_p[j][idx] += 1
+            elif id in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                false_n[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                false_p[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                true_n[j][idx] += 1
+    j += 1
+    print(f"Species {j} done.")
+    
+
+
+true_p_rate = true_p/(true_p + false_n)
+false_p_rate = false_p/(true_n + false_p)
+precision = true_p/(true_p+false_p)
+recall = true_p_rate
+
+AUC = []
+
+AUC.append(np.abs(np.trapz(y=true_p_rate[0].tolist(), x=false_p_rate[0].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[1].tolist(), x=false_p_rate[1].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[2].tolist(), x=false_p_rate[2].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[3].tolist(), x=false_p_rate[3].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[4].tolist(), x=false_p_rate[4].tolist())))
+
+print('ROC AUC values are: ', AUC)
+
+mean_AUC_ROC = sum(AUC)/len(AUC)
+
+print('Mean ROC AUC is', mean_AUC_ROC)
+
+PR_AUC = []
+
+PR_AUC.append(np.abs(np.trapz(y=precision[0].tolist(), x=recall[0].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[1].tolist(), x=recall[1].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[2].tolist(), x=recall[2].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[3].tolist(), x=recall[3].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[4].tolist(), x=recall[4].tolist())))
+
+print('PR AUC values are: ', PR_AUC)
+
+mean_AUC_PR = sum(PR_AUC)/len(PR_AUC)
+
+print('Mean PR AUC is', mean_AUC_PR)
+
+sns.barplot(x=small_dist, y=AUC, color='b')########
+plt.xlabel('Species')
+plt.ylabel('AUC-ROC')
+plt.show()
+
+sns.barplot(x=small_dist, y=PR_AUC, color='r')########
+plt.xlabel('Species')
+plt.ylabel('AUC-Precision-Recall')
+plt.ylim(0, 1)
+plt.show()
+
+sns.barplot(x=['mean_AUC_ROC', 'mean_AUC_PR'], y=[mean_AUC_ROC, mean_AUC_PR], color='r')
+plt.xlabel('Smallest Distance Averages')###############
+plt.ylabel('AUC')
+plt.ylim(0, 1)
+plt.show()
+
+true_p = np.zeros((5, 20))
+true_n = np.zeros((5, 20))
+false_p = np.zeros((5, 20))
+false_n = np.zeros((5, 20))
+total = np.zeros((5, 20))
+
+j = 0
+for id in most_sparse: #####
+    id_inx = np.where(species == id)
+    for i in range(len(test_locs)):
+        for idx, thr in enumerate(np.linspace(0.0, 0.05, 20)): #Maybe use 4 or 5 to start?
+            if id in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                true_p[j][idx] += 1
+            elif id in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                false_n[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                false_p[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                true_n[j][idx] += 1
+    j += 1
+    print(f"Species {j} done.")
+    
+
+
+true_p_rate = true_p/(true_p + false_n)
+false_p_rate = false_p/(true_n + false_p)
+precision = true_p/(true_p+false_p)
+recall = true_p_rate
+
+AUC = []
+
+AUC.append(np.abs(np.trapz(y=true_p_rate[0].tolist(), x=false_p_rate[0].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[1].tolist(), x=false_p_rate[1].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[2].tolist(), x=false_p_rate[2].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[3].tolist(), x=false_p_rate[3].tolist())))
+AUC.append(np.abs(np.trapz(y=true_p_rate[4].tolist(), x=false_p_rate[4].tolist())))
+
+print('ROC AUC values are: ', AUC)
+
+mean_AUC_ROC = sum(AUC)/len(AUC)
+
+print('Mean ROC AUC is', mean_AUC_ROC)
+
+PR_AUC = []
+
+PR_AUC.append(np.abs(np.trapz(y=precision[0].tolist(), x=recall[0].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[1].tolist(), x=recall[1].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[2].tolist(), x=recall[2].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[3].tolist(), x=recall[3].tolist())))
+PR_AUC.append(np.abs(np.trapz(y=precision[4].tolist(), x=recall[4].tolist())))
+
+print('PR AUC values are: ', PR_AUC)
+
+mean_AUC_PR = sum(PR_AUC)/len(PR_AUC)
+
+print('Mean PR AUC is', mean_AUC_PR)
+
+sns.barplot(x=most_sparse, y=AUC, color='b')########
+plt.xlabel('Species')
+plt.ylabel('AUC-ROC')
+plt.show()
+
+sns.barplot(x=most_sparse, y=PR_AUC, color='r')########
+plt.xlabel('Species')
+plt.ylabel('AUC-Precision-Recall')
+plt.ylim(0, 1)
+plt.show()
+
+sns.barplot(x=['mean_AUC_ROC', 'mean_AUC_PR'], y=[mean_AUC_ROC, mean_AUC_PR], color='r')
+plt.xlabel('Most Sparse averages')###############
+plt.ylabel('AUC')
+plt.ylim(0, 1)
+plt.show()
+
+true_p = np.zeros((500, 20))
+true_n = np.zeros((500, 20))
+false_p = np.zeros((500, 20))
+false_n = np.zeros((500, 20))
+total = np.zeros((500, 20))
+
+j = 0
+for id in species: #####
+    id_inx = np.where(species == id)
+    for i in range(len(test_locs)):
+        for idx, thr in enumerate(np.linspace(0.0, 0.05, 20)): #Maybe use 4 or 5 to start?
+            if id in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                true_p[j][idx] += 1
+            elif id in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                false_n[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] > thr:
+                false_p[j][idx] += 1
+            elif id not in test_ids[i] and predictions_p[i][id_inx[0]] < thr:
+                true_n[j][idx] += 1
+    j += 1
+    print(f"Species {j} done.")
+    
+
+
+true_p_rate = true_p/(true_p + false_n)
+false_p_rate = false_p/(true_n + false_p)
+precision = true_p/(true_p+false_p)
+recall = true_p_rate
+
+AUC = []
+
+for i in range(len(species)):
+    AUC.append(np.abs(np.trapz(y=true_p_rate[i].tolist(), x=false_p_rate[i].tolist())))
+
+#AUC.append(np.abs(np.trapz(y=true_p_rate[0].tolist(), x=false_p_rate[0].tolist())))
+#AUC.append(np.abs(np.trapz(y=true_p_rate[1].tolist(), x=false_p_rate[1].tolist())))
+#AUC.append(np.abs(np.trapz(y=true_p_rate[2].tolist(), x=false_p_rate[2].tolist())))
+#AUC.append(np.abs(np.trapz(y=true_p_rate[3].tolist(), x=false_p_rate[3].tolist())))
+#AUC.append(np.abs(np.trapz(y=true_p_rate[4].tolist(), x=false_p_rate[4].tolist())))
+
+print('ROC AUC values are: ', AUC)
+
+mean_AUC_ROC = sum(AUC)/len(AUC)
+
+print('Mean ROC AUC is', mean_AUC_ROC)
+
+PR_AUC = []
+
+for i in range(len(species)):
+    PR_AUC.append(np.abs(np.trapz(y=precision[i].tolist(), x=recall[i].tolist())))
+
+#PR_AUC.append(np.abs(np.trapz(y=precision[0].tolist(), x=recall[0].tolist())))
+#PR_AUC.append(np.abs(np.trapz(y=precision[1].tolist(), x=recall[1].tolist())))
+#PR_AUC.append(np.abs(np.trapz(y=precision[2].tolist(), x=recall[2].tolist())))
+#PR_AUC.append(np.abs(np.trapz(y=precision[3].tolist(), x=recall[3].tolist())))
+#PR_AUC.append(np.abs(np.trapz(y=precision[4].tolist(), x=recall[4].tolist())))
+
+print('PR AUC values are: ', PR_AUC)
+
+mean_AUC_PR = sum(PR_AUC)/len(PR_AUC)
+
+print('Mean PR AUC is', mean_AUC_PR)
+
+sns.barplot(x=['mean_AUC_ROC', 'mean_AUC_PR'], y=[mean_AUC_ROC, mean_AUC_PR], color='r')
+plt.xlabel('TOTAL AVERAGES')###############
+plt.ylabel('AUC')
+plt.ylim(0, 1)
+plt.show()
 
 """
+#prec1 = precision[2].tolist()
+#rec1 = recall[2].tolist()
+#prec2 = precision[3].tolist()
+#rec2 = recall[3].tolist()
+
+#test_locs_id = test_pos_inds.get(35990)
+#print(test_locs_id)
+#print(prec1)
+#print(true_p_rate[2].tolist())
+#print(rec1)
+
+#plt.plot(rec1, prec1)
+#plt.show()
+
+#plt.plot(rec2, prec2)
+#plt.show()
+
 
 id = 12716
 tp = 0
