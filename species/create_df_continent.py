9ae468966a327ecf4996b72154feec239d9eaad7
miguelpmerelles <miguelperezmerelles8@gmail.com>
2023-11-15 15:06:41 +0000

    commit

--- species/create_df_continent.py
+++ species/create_df_continent.py
@@ -22,6 +22,57 @@ import pandas as pd
 from collections import Counter
 
 
+# Read the CSV file into a DataFrame
+df = pd.read_csv("all_species_continent_data.csv")
+
+data = np.load('species_train.npz')
+train_locs = data['train_locs']          
+train_ids = data['train_ids']               
+species = data['taxon_ids']      
+species_names = dict(zip(data['taxon_ids'], data['taxon_names'])) 
+
+_, species_counts = np.unique(train_ids, return_counts=True)
+species_count = np.bincount(train_ids)
+
+top500_sp_list = []
+
+for id in species:
+    index = np.where(train_ids == id)[0]
+    count = len(index)
+    #print(count)
+    if count > 0 and count <= 500:
+        top500_sp_list.append(id)
+
+
+filter1 = df[df['Species ID'].isin(top500_sp_list)]
+
+
+
+
+
+
+# Use value_counts() to get the count of rows per continent
+continent_counts = filter1['Continent'].value_counts()
+
+# Print or use the counts as needed
+print(continent_counts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Below the code to create the continent data 
+
 def get_continent_name(continent_code: str) -> str:
     continent_dict = {
         "NA": "North America",
@@ -64,64 +115,6 @@ def get_continent(lat: float, lon: float) -> Tuple[str, str]:
         print("KeyError: 'country_code' not found in address")
         return "Unknown", "Unknown"
 
-"""
-def get_continent(lat: float, lon:float) -> Tuple[str, str]:
-    geolocator = Nominatim(user_agent="<username>@gmail.com", timeout=10)
-    geocode = RateLimiter(geolocator.reverse, min_delay_seconds=1)
-
-    location = geocode(f"{lat}, {lon}", language="en")
-
-    # for cases where the location is not found, coordinates ASSUME antarctica!!!!!!!!!!!!!!??? Could be sea!
-    if location is None:
-        return "Antarctica", "Antarctica"
-
-    # extract country code
-    address = location.raw["address"]
-    country_code = address["country_code"].upper()
-
-    if country_code == 'TL':
-        continent_name = 'AS'
-        return continent_name
-    elif country_code:
-        continent_code = pc.country_alpha2_to_continent_code(country_code)
-        continent_name = get_continent_name(continent_code)
-        return continent_name
-    else:
-        return "Antarctica", "Antarctica"
-    
-
-    # get continent code from country code
-    #continent_code = pc.country_alpha2_to_continent_code(country_code) #Replaced these 3 lines for following code
-    #continent_name = get_continent_name(continent_code)
-    #return continent_name
-"""
-"""
-        try:
-        continent_name = pc.country_alpha2_to_continent_code(country_code)
-
-    except Exception as e:
-        # Special Case: Timor Leste
-        if e == "Invalid Country Alpha-2 code: \'TL\'":
-            continent_name = 'AS'
-        else:
-            continent_name = 'N/A'
-    
-    if country_code:
-        continent_code = pc.country_alpha2_to_continent_code(country_code)
-        try:
-            continent_name = get_continent_name(continent_code)
-        except Exception as e:
-            if str(e) == '"Invalid Country Alpha-2 code: \'TL\'"':
-                continent_name = 'AS'
-            else:
-                continent_name = 'N/A'
-        return continent_name
-    
-    else:
-        # Handle unrecognized country code, assuming it's Antarctica
-        return "Antarctica", "Antarctica"
-"""
-
 
 data = np.load('species_train.npz')
 train_locs = data['train_locs']          
@@ -131,26 +124,10 @@ species_names = dict(zip(data['taxon_ids'], data['taxon_names']))
 
 species_df = pd.DataFrame(columns=['Species ID', 'Species Name', 'Continent']) #Should I add the number of 
 
-#_, species_counts = np.unique(train_ids, return_counts=True)
-#species_count = np.bincount(train_ids)
-
-#all_sp_list = []
-#i = 0
-
-#for n in species_count:
-#    if n < species_counts.max():
- #       all_sp_list.append(i)
-  #  i = i + 1
-
-#print(len(all_sp_list))
 
 train_inds_pos = []
 for n in species:
     train_inds_pos.append(np.where(train_ids == n)[0])
-
-#print(len(train_inds_pos))
-#print(train_inds_pos[0])
-#print(train_inds_pos[1]) 
 
 i = 0
 for species_indices in train_inds_pos:
@@ -172,12 +149,11 @@ for species_indices in train_inds_pos:
     #species_df = species_df.append({'Species ID': species_idf, 'Species Name': species_namef, 'Continent': most_common_continent}, ignore_index=True)
     species_df = pd.concat([species_df, pd.DataFrame([{'Species ID': species_idf, 'Species Name': species_namef, 'Continent': most_common_continent}])], ignore_index=True)
 
-    #df = pd.DataFrame(df).append(new_row, ignore_index=True)
-    #df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
     print('Done species :', i)
-    #if i > 100:
-    #    break
+
 
 # Save the species_df DataFrame to a CSV file
 species_df.to_csv('all_species_continent_data.csv', index=False)
 
+"""
